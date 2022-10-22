aae1e55921084a422e9faad61de42b7a7b146ba4
Jakub Gałek <xwondunx@gmail.com>
2022-10-22 21:28:32 +0200

    fixing

--- sketch_oct15a.ino
+++ sketch_oct15a.ino
@@ -1,52 +1,18 @@
-/*------------------------------------------------------------------------
-Example sketch for Adafruit Thermal Printer library for Arduino.
-Demonstrates a few text styles & layouts, bitmap printing, etc.
- 
-IMPORTANT: DECLARATIONS DIFFER FROM PRIOR VERSIONS OF THIS LIBRARY.
-This is to support newer & more board types, especially ones that don't
-support SoftwareSerial (e.g. Arduino Due). You can pass any Stream
-(e.g. Serial1) to the printer constructor. See notes below.
-------------------------------------------------------------------------*/
- 
-#include "Adafruit_Thermal.h"
- 
-// Here's the new syntax when using SoftwareSerial (e.g. Arduino Uno) ----
-// If using hardware serial instead, comment out or remove these lines:
- 
-#include "SoftwareSerial.h"
-#define TX_PIN 6 // Arduino transmit YELLOW WIRE labeled RX on printer
-#define RX_PIN 5 // Arduino receive GREEN WIRE labeled TX on printer
- 
-//SoftwareSerial mySerial(RX_PIN, TX_PIN); // Declare SoftwareSerial obj first
-//Adafruit_Thermal printer(&mySerial); // Pass addr to printer constructor
-// Then see setup() function regarding serial & printer begin() calls.
- 
-// Here's the syntax for hardware serial (e.g. Arduino Due) --------------
-// Un-comment the following line if using hardware serial:
- 
-//Adafruit_Thermal printer(&Serial1); // Or Serial2, Serial3, etc.
- 
-// -----------------------------------------------------------------------
-//const int btnPin = A0; // le bouton est connecté à la broche A0 de la carte Adruino
-//int btnVal = 0;
-
-
 /********************************************************************/
-// First we include the libraries
-//#include <OneWire.h> 
-//#include <DallasTemperature.h>
+#include <OneWire.h> 
+#include <DallasTemperature.h>
 /********************************************************************/
 // Data wire is plugged into pin 2 on the Arduino 
-//#define ONE_WIRE_BUS 2 
+#define ONE_WIRE_BUS 2 
 /********************************************************************/
 // Setup a oneWire instance to communicate with any OneWire devices  
 // (not just Maxim/Dallas temperature ICs) 
-//OneWire oneWire(ONE_WIRE_BUS); 
+OneWire oneWire(ONE_WIRE_BUS); 
 /********************************************************************/
 // Pass our oneWire reference to Dallas Temperature. 
-//DallasTemperature sensors(&oneWire);
+DallasTemperature sensors(&oneWire);
 /********************************************************************/ 
-//#include <Wire.h>
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 /********************************************************************/ 
@@ -69,12 +35,12 @@ unsigned long roznicaCzasu = 0;
  //FLAGA INFORMUJACA WYSWIETLACZ CZY KARTA JEST WLOZONA
  int flaga = 0;
 /********************************************************************/     
- float Temp_A = 23;
- float Temp_B = 21;
- float Temp_C = 13;
- float Temp_D = 23;
- float Temp_E = 13;
- float Temp_F = 33;
+ float Temp_A = 00;
+ float Temp_B = 00;
+ float Temp_C = 00;
+ float Temp_D = 00;
+ float Temp_E = 00;
+ float Temp_F = 00;
 /********************************************************************/ 
 
  int co_ile_minut_zapis = 15;
@@ -90,47 +56,26 @@ unsigned long roznicaCzasu = 0;
 /********************************************************************/
  
 void setup() {
-//pinMode(btnPin,INPUT_PULLUP);
- 
-// This line is for compatibility with the Adafruit IotP project pack,
-// which uses pin 7 as a spare grounding point. You only need this if
-// wired up the same way (w/3-pin header into pins 5/6/7):
-//pinMode(7, OUTPUT); digitalWrite(7, LOW);
- 
-// NOTE: SOME PRINTERS NEED 9600 BAUD instead of 19200, check test page.
-//mySerial.begin(9600); // Initialize SoftwareSerial
-//Serial1.begin(19200); // Use this instead if using hardware serial
-//printer.begin(); // Init printer (same regardless of serial type)
- 
-// The following calls are in setup(), but don't *need* to be. Use them
-// anywhere! They're just here so they run one time and are not printed
-// over and over (which would happen if they were in loop() instead).
-// Some functions will feed a line when called, this is normal.
-
 
  // start serial port 
  Serial.begin(9600); 
  Serial.println("Dallas Temperature IC Control Library Demo"); 
 /********************************************************************/
-  
-  
  // Start up the library 
-// sensors.begin(); 
+ sensors.begin(); 
 /********************************************************************/ 
  lcd.init();   
  lcd.backlight(); // zalaczenie podwietlenia 
 /********************************************************************/ 
  if (!SD.begin(10)) {
     Serial.println("initialization SD failed!");
-        lcd.println("WLOZ KARTE!");
+    lcd.println("_BRAK KARTY_");
     lcd.setCursor(0,1); 
-    lcd.println("I ZRESETUJ UKLAD");
+    lcd.println("_PRZY STARCIE_");
     while (1);
   }
   Serial.println("initialization SD done.");
  /********************************************************************/
-
-
  // Initialize DS3231
   Serial.println("Initialize DS3231");;
   clock.begin();
@@ -142,13 +87,11 @@ void setup() {
 
  dt = clock.getDateTime();
  
- 
   myFile = SD.open("LOGS.txt", FILE_WRITE);
 
     // if the file opened okay, write to it:
     if (myFile) {
     Serial.print("Writing to logs.txt...");
-
 
   myFile.print(dt.year);   myFile.print("-");
   myFile.print(dt.month);  myFile.print("-");
@@ -157,15 +100,15 @@ void setup() {
   myFile.print(dt.minute); myFile.print(":");
   myFile.print(dt.second); myFile.print("      ");
     
-   myFile.print(Temp_A); myFile.print("      ");     
-   myFile.print(Temp_B); myFile.print("      ");
-   myFile.print(Temp_C); myFile.print("      ");
-   myFile.print(Temp_D); myFile.print("      ");
-   myFile.print(Temp_E); myFile.print("      ");
-   myFile.print(Temp_F); myFile.println("");
+  myFile.print(Temp_A); myFile.print("      ");     
+  myFile.print(Temp_B); myFile.print("      ");
+  myFile.print(Temp_C); myFile.print("      ");
+  myFile.print(Temp_D); myFile.print("      ");
+  myFile.print(Temp_E); myFile.print("      ");
+  myFile.print(Temp_F); myFile.println("");
 
-        Serial.print("File size: ");
-        Serial.println(myFile.size());
+      Serial.print("File size: ");
+      Serial.println(myFile.size());
 
       myFile.close();
       Serial.println("done.");
@@ -180,8 +123,11 @@ void loop() {
  // request to all devices on the bus 
 /********************************************************************/
  //Serial.print(" Requesting temperatures..."); 
-// sensors.requestTemperatures(); // Send the command to get temperature readings 
-// Serial.println("DONE"); 
+ sensors.requestTemperatures(); // Send the command to get temperature readings 
+ //Serial.println("DONE"); 
+
+ Temp_A = sensors.getTempCByIndex(0);
+ Temp_B = sensors.getTempCByIndex(1);
 /********************************************************************/
  if(flaga==0){
 lcd.setCursor(0,0); 
@@ -222,18 +168,18 @@ lcd.setCursor(0,0);
   myFile.print(dt.minute); myFile.print(":");
   myFile.print(dt.second); myFile.print("      ");
     
-   myFile.print(Temp_A); myFile.print("      ");     
-   myFile.print(Temp_B); myFile.print("      ");
-   myFile.print(Temp_C); myFile.print("      ");
-   myFile.print(Temp_D); myFile.print("      ");
-   myFile.print(Temp_E); myFile.print("      ");
-   myFile.print(Temp_F); myFile.println("");
+  myFile.print(Temp_A); myFile.print("      ");     
+  myFile.print(Temp_B); myFile.print("      ");
+  myFile.print(Temp_C); myFile.print("      ");
+  myFile.print(Temp_D); myFile.print("      ");
+  myFile.print(Temp_E); myFile.print("      ");
+  myFile.print(Temp_F); myFile.println("");
 
         Serial.print("File size: ");
         Serial.println(myFile.size());
 
 
-            myFile.close();
+      myFile.close();
       Serial.println("close file done.");
     
          if (myFile.size()>=rozmiarpoczasie)
@@ -242,9 +188,6 @@ lcd.setCursor(0,0);
          SD.remove("LOGS.txt");
       } 
       
-  
-
-
   } else {
     // if the file didn't open, print an error:
     Serial.println("error opening logs.txt");
@@ -255,7 +198,7 @@ lcd.setCursor(0,0);
 if(flaga==1)
 {
     lcd.setCursor(0,0); 
-    lcd.println("WLOZ KARTE!");
+    lcd.println("_WLOZ KARTE_");
 
     lcd.setCursor(0,1); 
     lcd.println("I ZRESETUJ UKLAD");
@@ -269,14 +212,4 @@ if(flaga==1)
 }
  /********************************************************************/
 
-//btnVal=analogRead(btnPin);
-//if(btnVal<200){ // We press the push button
-//printer.print(("Temp_A: ")); printer.println((Temp_A));
-//printer.print(("Temp_B: ")); printer.println((Temp_B));
-//printer.print(("Temp_C: ")); printer.println((Temp_C));
-//printer.print(("Temp_D: ")); printer.println((Temp_D));
-//printer.print(("Temp_E: ")); printer.println((Temp_E));
-//printer.print(("Temp_F: ")); printer.println((Temp_F));   
-//}
- /********************************************************************/
 }
